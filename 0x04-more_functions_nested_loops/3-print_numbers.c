6cbe6ef56adb1547def6f704af16dc300f84070e
Aaron <aaronmulwa@yahoo.com>
2023-02-23 14:12:36 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 -9
+ * Return: nada
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
